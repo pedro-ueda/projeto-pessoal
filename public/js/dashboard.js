f4f82ccb1e6f92cf61445444efd205528445afce
pedro-ueda <pedro.ueda@sptech.school>
2024-05-17 15:51:12 -0300

    arrumando permissão para o questionario

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -2,10 +2,13 @@ var respostasUsuarios = window.sessionStorage.getItem("respostasUsuarios").split
 
 function mostrarRespostasDoUsuario() {
     for (let index = 0; index < respostasUsuarios.length; index++) {
-        p_respostasDosUsuario.innerHTML += `Pergunta ${index + 1}: ${respostasUsuarios[index]}<br>`;
+        p_respostasDosUsuario.innerHTML += `Resposta ${index + 1}: ${respostasUsuarios[index]}<br>`;
 
     }
 }
 
-
 mostrarRespostasDoUsuario();
+
+if(respostasUsuarios[0] == "Mulher") {
+
+}
--- public/js/quiz.js
+++ public/js/quiz.js
@@ -1,11 +1,16 @@
-var respostasUsuarios = [];
-var praticaMusculacao;
+var email = sessionStorage.getItem('EMAIL_USUARIO');
+console.log(email);
+if (email == "null") {
+    div_msg.innerHTML = "Você deve ter um login para ter acesso ao questionário!";
+} else {
+    var respostasUsuarios = [];
+    var sentePerdido;
 
-function mostrarQuiz() {
-    
-    div_msg.innerHTML = "";
-        
-    div_msg.innerHTML = `<div class="box-titulo flex">
+    function mostrarQuiz() {
+
+        div_msg.innerHTML = "";
+
+        div_msg.innerHTML = `<div class="box-titulo flex">
     <h1 class="inter-titulo titulo-quiz">1. Pergunta</h1>
     </div>
     
@@ -15,28 +20,29 @@ function mostrarQuiz() {
     <option value="#">Selecione seu gênero</option>
     <option value="Homem">Homem</option>
     <option value="Mulher">Mulher</option>
+    <option value="Outros">Outros</option>
     </select>
     
     <button class="enviar sombra inter-titulo" onclick="pergunta2()">Enviar</button>
     </div>`;
-    
-    
-}
 
-function pergunta2() {
-    
-    if (sl_genero.value == "#") {
-        div_msg.innerHTML += `<br> <span class="inter-p">Esta não é uma opção válida!</span>`;
-    } else {
-        respostasUsuarios.push(sl_genero.value);
-        
-        div_msg.innerHTML = `<div class="box-titulo flex">
+
+    }
+
+    function pergunta2() {
+
+        if (sl_genero.value == "#") {
+            div_msg.innerHTML += `<br> <span class="inter-p">Esta não é uma opção válida!</span>`;
+        } else {
+            respostasUsuarios.push(sl_genero.value);
+
+            div_msg.innerHTML = `<div class="box-titulo flex">
         <h1 class="inter-titulo titulo-quiz">2. Pergunta</h1>
         </div>
         
-        <p class="inter-p">Você pratica musculação?</p>
+        <p class="inter-p">Você se sente meio perdido, ou fora do contexto em alguns momentos do seu dia a dia?</p>
             
-        <select id="sl_praticaMusculacao" class="sombra">
+        <select id="sl_sentePerdido" class="sombra">
         <option value="#">Selecione uma opção</option>
         <option>Sim</option>
         <option>Não</option>
@@ -44,159 +50,155 @@ function pergunta2() {
         
         <button class="enviar sombra inter-titulo" onclick="pergunta3()">Enviar</button>
         </div>`;
+        }
     }
-}
 
-function pergunta3() {
-    praticaMusculacao = sl_praticaMusculacao.value;
-    
-    if (praticaMusculacao == "#") {
-        div_msg.innerHTML += `<br> <span class="inter-p">Esta não é uma opção válida!</span>`;
-    } else if(praticaMusculacao == "Sim"){
-        respostasUsuarios.push(praticaMusculacao);
-        
-        div_msg.innerHTML = `<div class="box-titulo flex">
+    function pergunta3() {
+        sentePerdido = sl_sentePerdido.value;
+
+        if (sentePerdido == "#") {
+            div_msg.innerHTML += `<br> <span class="inter-p">Esta não é uma opção válida!</span>`;
+        } else {
+            respostasUsuarios.push(sentePerdido);
+
+            div_msg.innerHTML = `<div class="box-titulo flex">
         <h1 class="inter-titulo titulo-quiz">3. Pergunta</h1>
         </div>
         
-        <p class="inter-p">Há quanto tempo você treina?</p>
+        <p class="inter-p">Você acha que não consegue se concentrar nas suas atividades?</p>
         
-        <select id="sl_tempoDePratica" class="sombra">
+        <select id="sl_concentrado" class="sombra">
         <option value="#">Selecione uma opção</option>
-        <option>Menos de 1 ano</option>
-        <option>Entre 1 ano e 3 anos</option>
-        <option>4 anos ou mais</option>
+        <option>Sim</option>
+        <option>Não</option>
         </select>
         
         <button class="enviar sombra inter-titulo" onclick="pergunta4()">Enviar</button>
         </div>`;
-        
-    }else {
-        respostasUsuarios.push(praticaMusculacao);
-        mandarParaDash();
-    }
-    
-    
-}
 
-function pergunta4() {
-    
-    if (sl_tempoDePratica.value == "#") {
-        div_msg.innerHTML += `<br> <span class="inter-p">Esta não é uma opção válida!</span>`;
-    } else {
-        respostasUsuarios.push(sl_tempoDePratica.value);
-        
-        div_msg.innerHTML = `
+        }
+
+
+    }
+
+    function pergunta4() {
+
+        if (sl_concentrado.value == "#") {
+            div_msg.innerHTML += `<br> <span class="inter-p">Esta não é uma opção válida!</span>`;
+        } else {
+            respostasUsuarios.push(sl_concentrado.value);
+
+            div_msg.innerHTML = `
         <div class="box-titulo flex">
         <h1 class="inter-titulo titulo-quiz">4. Pergunta</h1>
         </div>
         
-        <p class="inter-p">Por que você começou a treinar?</p>
+        <p class="inter-p">Você geralmente passa por momentos de indisposião ao longo do seu cotidiano?</p>
         
-        <select id="sl_motivo" class="sombra">
+        <select id="sl_indisposicao" class="sombra">
         <option value="#">Selecione uma opção</option>
-        <option>Saúde</option>
-        <option>Estética</option>
-        <option>Outros</option>
+        <option>Sim</option>
+        <option>Não</option>
         </select>
         
         <button class="enviar sombra inter-titulo" onclick="pergunta5()">Enviar</button>
         </div>`;
-        
-    }
-}
 
-function pergunta5() {
-    
-    if (sl_motivo.value == "#") {
-        div_msg.innerHTML += `<br> <span class="inter-p">Esta não é uma opção válida!</span>`;
-    } else {
-        respostasUsuarios.push(sl_motivo.value);
-        
-        div_msg.innerHTML = `
+        }
+    }
+
+    function pergunta5() {
+
+        if (sl_indisposicao.value == "#") {
+            div_msg.innerHTML += `<br> <span class="inter-p">Esta não é uma opção válida!</span>`;
+        } else {
+            respostasUsuarios.push(sl_indisposicao.value);
+
+            div_msg.innerHTML = `
         <div class="box-titulo flex">
         <h1 class="inter-titulo titulo-quiz">5. Pergunta</h1>
         </div>
         
-            <p class="inter-p">Qual membro você prefere treinar?</p>
+            <p class="inter-p">Você acredita que seu foco poderia ser melhor?</p>
             
-            <select id="sl_membro" class="sombra">
+            <select id="sl_focado" class="sombra">
             <option value="#">Selecione uma opção</option>
-            <option>Inferiores</option>
-            <option>Superiores</option>
+            <option>Sim</option>
+            <option>Não</option>
             </select>
             
             <button class="enviar sombra inter-titulo" onclick="pergunta6()">Enviar</button>
             </div>`;
-            
+
         }
-        
+
     }
-    
+
     var autoEstimaAntes;
-    
+
     function pergunta6() {
-        
-        if (sl_membro.value == "#") {
+
+        if (sl_focado.value == "#") {
             div_msg.innerHTML += `<br> <span class="inter-p">Esta não é uma opção válida!</span>`;
         } else {
-            respostasUsuarios.push(sl_membro.value);
-            
+            respostasUsuarios.push(sl_focado.value);
+
             div_msg.innerHTML = `
             <div class="box-titulo flex">
             <h1 class="inter-titulo titulo-quiz">6. Pergunta</h1>
             </div>
             
-            <p class="inter-p">Qual nota você daria para sua saúde mental (autoestima, disposição, etc) <b>antes</b> de começar a treinar? (de 0 a 10)</p>
+            <p class="inter-p">Qual nota você daria para sua saúde mental (autoestima, disposição, etc)? (de 0 a 10)</p>
             
             <input id="input_autoEstimaAntes" class="input-questionario sombra" type="number" placeholder="Insira aqui sua nota">
             
             <button class="enviar sombra inter-titulo" onclick="pergunta7()">Enviar</button>`;
-            
+
         }
     }
-    
+
     function pergunta7() {
-        
-        if(input_autoEstimaAntes.value == "") {
+
+        if (input_autoEstimaAntes.value == "") {
             div_msg.innerHTML += `<br> <span class="inter-p">É necessário inserir algum valor para prosseguir!</span>`;
-        }else if(input_autoEstimaAntes.value >= 0 && input_autoEstimaAntes.value <= 10){ 
+        } else if (input_autoEstimaAntes.value >= 0 && input_autoEstimaAntes.value <= 10) {
             autoEstimaAntes = input_autoEstimaAntes.value;
-            
+
             respostasUsuarios.push(autoEstimaAntes);
-            
+
             div_msg.innerHTML = `
             <div class="box-titulo flex">
             <h1 class="inter-titulo titulo-quiz">7. Pergunta</h1>
             </div>
             
-            <p class="inter-p">Qual nota você daria para sua saúde mental (autoestima, disposição, etc) <b>depois</b> de começar a treinar? (de 0 a 10)</p>
+            <p class="inter-p">Qual nota você daria para uma saúde mental (autoestima, disposição, etc) boa? (de 0 a 10)</p>
             
             <input id="input_autoEstimaDepois" class="input-questionario sombra" type="number" placeholder="Insira aqui sua nota">
             
                 <button class="enviar sombra inter-titulo" onclick="mostrarDashboard()">Enviar</button>`;
-            }else {
-                div_msg.innerHTML += `<br> <span class="inter-p">Este valor não é válido!</span>`;
-            }
+        } else {
+            div_msg.innerHTML += `<br> <span class="inter-p">Este valor não é válido!</span>`;
         }
-        var autoEstimaDepois;
-        function mostrarDashboard() {
-            if(input_autoEstimaDepois.value == "") {
-                div_msg.innerHTML += `<br> <span class="inter-p">É necessário inserir algum valor para prosseguir!</span>`;
-            }else if(input_autoEstimaDepois.value >= 0 && input_autoEstimaDepois.value <= 10){
-                autoEstimaDepois = input_autoEstimaDepois.value;
-                
-                respostasUsuarios.push(autoEstimaDepois);
-                
-                mandarParaDash()
-                
-            }else {
-                div_msg.innerHTML += `<br> <span class="inter-p">Este valor não é válido!</span>`;
-            }
-        }
+    }
+    var autoEstimaDepois;
+    function mostrarDashboard() {
+        if (input_autoEstimaDepois.value == "") {
+            div_msg.innerHTML += `<br> <span class="inter-p">É necessário inserir algum valor para prosseguir!</span>`;
+        } else if (input_autoEstimaDepois.value >= 0 && input_autoEstimaDepois.value <= 10) {
+            autoEstimaDepois = input_autoEstimaDepois.value;
 
-        
-        function mandarParaDash() {
-            window.location.href = 'dashboard.html';
-            window.sessionStorage.setItem('respostasUsuarios', respostasUsuarios);
+            respostasUsuarios.push(autoEstimaDepois);
+
+            mandarParaDash()
+
+        } else {
+            div_msg.innerHTML += `<br> <span class="inter-p">Este valor não é válido!</span>`;
         }
+    }
+
+
+    function mandarParaDash() {
+        window.location.href = 'dashboard.html';
+        window.sessionStorage.setItem('respostasUsuarios', respostasUsuarios);
+    }
+}
